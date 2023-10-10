fdfdf7aff65ed52d67c41566c403308577eb66ec
JustPtrck <patrickverwimp@gmail.com>
2023-10-10 23:34:45 +0200

    Added Models and some unused scripts

---
+++ RevSimVR/Assets/RevSim/Scripts/Interfaces.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable
+{
+    void Damage();
+}
+
+public interface IUseable
+{
+    void Interact();
+}
