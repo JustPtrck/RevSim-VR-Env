376076bdc14f07cc0a3d1447957e115611db056b
JustPtrck <patrickverwimp@gmail.com>
2023-11-27 14:43:23 +0100

    Test Scene updated for YAW

---
+++ RevSimV2/Assets/RevSim/SimulationManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YawVR;
+
+public class SimulationManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
